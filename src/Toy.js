e09bcb8fcc8c676f26fbbfad7a308261db63364a
ppp <piase1910@163.com>
2021-04-13 20:56:43 +0800

    [update]：style

--- src/Toy.js
+++ src/Toy.js
@@ -10,9 +10,24 @@ function trans(d) {
     return d;
 }
 
+function transFontSize(length){
+    if (length > 40){
+        return "14px";
+    }else if (length > 30){
+        return "17px";
+    }else if (length > 20){
+        return "19px";
+    }else if (length > 10){
+        return "22px";
+    }else {
+        return "24px";
+    }
+}
+
 function Toy() {
     const [date, setDate] = useState(new Date())
     const [motto, setMotto] = useState(localStorage.getItem("motto"))
+    const [fontSize, setFontSize] = useState("10px")
     setTimeout(() => {
         setDate(new Date());
     }, 1000);
@@ -21,6 +36,7 @@ function Toy() {
             if (res.status === 200) {
                 localStorage.setItem("motto", res.data)
                 setMotto(res.data);
+                setFontSize(transFontSize(res.data.length));
             }
         })
     }, []);
@@ -29,7 +45,7 @@ function Toy() {
         className="back center"
         style={{backgroundImage: `url(${backgroundImage1})`}}>
         <p className="clock">{`${trans(date.getHours())}:${trans(date.getMinutes())}`}</p>
-        <p className="motto" dangerouslySetInnerHTML={{__html: motto.replaceAll("\n", "</br>")}}/>
+        <p className="motto" style={{fontSize:fontSize}} dangerouslySetInnerHTML={{__html: motto.replaceAll("\n", "</br>")}}/>
     </div>
 }
 
