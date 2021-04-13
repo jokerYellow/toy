a728508a54eb12c9790458b1a673231f7a0da290
ppp <piase1910@163.com>
2021-04-13 20:38:43 +0800

    [add]:log

--- src/Toy.js
+++ src/Toy.js
@@ -1,14 +1,35 @@
 import './Toy.css';
 import backgroundImage1 from './wp1854626-the-witcher-3-wallpapers.jpg';
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+function trans(d) {
+    if (d < 10) {
+        return `0${d}`;
+    }
+    return d;
+}
 
 function Toy() {
+    const [date, setDate] = useState(new Date())
+    const [motto, setMotto] = useState(localStorage.getItem("motto"))
+    setTimeout(() => {
+        setDate(new Date());
+    }, 1000);
+    useEffect(() => {
+        axios.get("/motto/today").then(res => {
+            if (res.status === 200) {
+                localStorage.setItem("motto", res.data)
+                setMotto(res.data);
+            }
+        })
+    }, []);
+
     return <div
-    className="back center" 
-    style={{backgroundImage:`url(${backgroundImage1})`}}>
-        <div>
-            <p className="clock">18:47</p>
-            <p className="motto">stay foolish,stay hungry</p>
-        </div>
+        className="back center"
+        style={{backgroundImage: `url(${backgroundImage1})`}}>
+        <p className="clock">{`${trans(date.getHours())}:${trans(date.getMinutes())}`}</p>
+        <p className="motto" dangerouslySetInnerHTML={{__html: motto.replaceAll("\n", "</br>")}}/>
     </div>
 }
 
